3b5e767352a36f2280db8a282d1d48c9b9bbf1cd
anthony <tony.anthony@hotmail.es>
2023-03-14 13:55:56 -0500

    Footer telefono arregaldo

--- src/componentes/UltimosCasos.js
+++ src/componentes/UltimosCasos.js
@@ -1,69 +1,84 @@
 import LetsTalk from "../componentes/LetsTalk"
 import Carousel from 'better-react-carousel'
+import React, { useState, useEffect } from 'react';
 
-function UltimosCasos(){
-    return(
-        <>
-                {/* <div className="tituloCasos" id="Ultimos-casos">
-                    <p>Últimos casos</p>
-                </div>
-                <div className="casosRow2">
-                    <div className="casosImg">
-                        <img src="./img/exito1.png" alt="Vehicentro" />
-                        <div className="desc">
-                            <p><strong>Conversión</strong> del <br></br>32% para clientes nuevos.<span></span></p>  
-                        </div>
-                    </div>
-                    
-                    <div className="casosImg">
-                        <img src="./img/exito2.png" alt="Vehicentro" />
-                        <div className="desc">
-                            <p><strong>Conversión</strong> del <br></br>32% para clientes nuevos.<span></span></p>
-                        </div>
-                    </div>
-                    
-                    <div className="casosImg">
-                        <img src="./img/exito3.png" alt="Vehicentro" />
-                        <div className="desc">
-                            <p><strong>Conversión</strong> del <br></br>32% para clientes nuevos.<span></span></p>
-                        </div>
-                    </div>
-                </div> */}
+function UltimosCasos() {
+  const [numCols, setNumCols] = useState(3); // set the initial number of columns to 3
 
-            <div className="tituloCasos" id="Ultimos-casos">
-                                <p>Últimos casos</p>
-            </div>
-            <div className="casosEBox">
-                <Carousel cols={3} rows={1} gap={30} loop>
-                        <Carousel.Item>
-                            <img width="100%" src="./img/exito1.png" alt="" />
-                            <p><strong>Conversión</strong> del <br></br>32% para clientes nuevos.<span></span></p>
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <img width="100%" src="./img/exito2.png" alt="" />
-                            <p><strong>Conversión</strong> del <br></br>32% para clientes nuevos.<span></span></p>
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <img width="100%" src="./img/exito3.png" alt="" />
-                            <p><strong>Conversión</strong> del <br></br>32% para clientes nuevos.<span></span></p>
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <img width="100%" src="./img/exito1.png" alt="" />
-                            <p><strong>Conversión</strong> del <br></br>32% para clientes nuevos.<span></span></p>
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <img width="100%" src="./img/exito1.png" alt="" />
-                            <p><strong>Conversión</strong> del <br></br>32% para clientes nuevos.<span></span></p>
-                        </Carousel.Item>
-                        <Carousel.Item>
-                            <img width="100%" src="./img/exito1.png" alt="" />
-                            <p><strong>Conversión</strong> del <br></br>32% para clientes nuevos.<span></span></p>
-                        </Carousel.Item>
-                </Carousel>
-            </div>
-            
-            <LetsTalk />
-        </>
-    )
+  // update the number of columns based on the screen width
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 768) { // adjust the breakpoint as needed
+        setNumCols(1);
+      } else {
+        setNumCols(3);
+      }
+    };
+    handleResize();
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return (
+    <>
+      <div className="tituloCasos" id="Ultimos-casos">
+        <p>Últimos casos</p>
+      </div>
+      <div className="casosEBox">
+        <Carousel cols={numCols} rows={1} gap={30} loop>
+          <Carousel.Item>
+            <img width="100%" src="./img/exito1.png" alt="" />
+            <p>
+              <strong>Conversión</strong> del <br />
+              32% para clientes nuevos.
+              <span></span>
+            </p>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img width="100%" src="./img/exito2.png" alt="" />
+            <p>
+              <strong>Conversión</strong> del <br />
+              32% para clientes nuevos.
+              <span></span>
+            </p>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img width="100%" src="./img/exito3.png" alt="" />
+            <p>
+              <strong>Conversión</strong> del <br />
+              32% para clientes nuevos.
+              <span></span>
+            </p>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img width="100%" src="./img/exito1.png" alt="" />
+            <p>
+              <strong>Conversión</strong> del <br />
+              32% para clientes nuevos.
+              <span></span>
+            </p>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img width="100%" src="./img/exito1.png" alt="" />
+            <p>
+              <strong>Conversión</strong> del <br />
+              32% para clientes nuevos.
+              <span></span>
+            </p>
+          </Carousel.Item>
+          <Carousel.Item>
+            <img width="100%" src="./img/exito1.png" alt="" />
+            <p>
+              <strong>Conversión</strong> del <br />
+              32% para clientes nuevos.
+              <span></span>
+            </p>
+          </Carousel.Item>
+        </Carousel>
+      </div>
+      <LetsTalk />
+    </>
+  );
 }
-export default UltimosCasos
+
+export default UltimosCasos;
