04698647aeae3ff96729b2569b495170fd1e1483
anthony <tony.anthony@hotmail.es>
2023-03-14 10:06:38 -0500

    Color menu lateral

--- src/componentes/UltimosCasos.js
+++ src/componentes/UltimosCasos.js
@@ -4,32 +4,35 @@ import Carousel from 'better-react-carousel'
 function UltimosCasos(){
     return(
         <>
+                {/* <div className="tituloCasos" id="Ultimos-casos">
+                    <p>Últimos casos</p>
+                </div>
+                <div className="casosRow2">
+                    <div className="casosImg">
+                        <img src="./img/exito1.png" alt="Vehicentro" />
+                        <div className="desc">
+                            <p><strong>Conversión</strong> del <br></br>32% para clientes nuevos.<span></span></p>  
+                        </div>
+                    </div>
+                    
+                    <div className="casosImg">
+                        <img src="./img/exito2.png" alt="Vehicentro" />
+                        <div className="desc">
+                            <p><strong>Conversión</strong> del <br></br>32% para clientes nuevos.<span></span></p>
+                        </div>
+                    </div>
+                    
+                    <div className="casosImg">
+                        <img src="./img/exito3.png" alt="Vehicentro" />
+                        <div className="desc">
+                            <p><strong>Conversión</strong> del <br></br>32% para clientes nuevos.<span></span></p>
+                        </div>
+                    </div>
+                </div> */}
+
             <div className="tituloCasos" id="Ultimos-casos">
-                <p>Últimos casos</p>
+                                <p>Últimos casos</p>
             </div>
-            <div className="casosRow2">
-                <div className="casosImg">
-                    <img src="./img/exito1.png" alt="Vehicentro" />
-                    <div className="desc">
-                        <p><strong>Conversión</strong> del <br></br>32% para clientes nuevos.<span></span></p>
-                    </div>
-                </div>
-                
-                <div className="casosImg">
-                    <img src="./img/exito2.png" alt="Vehicentro" />
-                    <div className="desc">
-                        <p><strong>Conversión</strong> del <br></br>32% para clientes nuevos.<span></span></p>
-                    </div>
-                </div>
-                
-                <div className="casosImg">
-                    <img src="./img/exito3.png" alt="Vehicentro" />
-                    <div className="desc">
-                        <p><strong>Conversión</strong> del <br></br>32% para clientes nuevos.<span></span></p>
-                    </div>
-                </div>
-            </div>
-            
             <div className="casosEBox">
                 <Carousel cols={3} rows={1} gap={30} loop>
                         <Carousel.Item>
