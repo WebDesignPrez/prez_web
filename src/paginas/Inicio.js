d30d58d287f3144a6b4fb9352d3fca2e910c0dc7
WebDesignPrez <web.design.prez@gmail.com>
2023-03-20 09:08:32 -0500

    video

--- src/paginas/Inicio.js
+++ src/paginas/Inicio.js
@@ -29,7 +29,7 @@ function Inicio() {
                 </div>
             </div>
             <div className='boxVideoInicio'>
-                <div className="videoInicio">
+                <div className="videoInicio noresponsive">
                 <iframe width="1820" height="800" src="https://www.youtube.com/embed/G87SGsIYATM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 
 
