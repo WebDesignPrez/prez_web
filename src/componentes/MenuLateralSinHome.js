c42b8f84a93277e731c1686d5700d0ff96ea1ecd
WebDesignPrez <web.design.prez@gmail.com>
2023-03-27 16:08:25 -0500

    cambios movil

--- src/componentes/MenuLateralSinHome.js
+++ src/componentes/MenuLateralSinHome.js
@@ -63,6 +63,7 @@ function MenuLateralSinHome() {
                 {/* <div className="logoLateral" onClick={topFunction}></div> */}
                 <a className="burger" onClick={handleToggle}> <span></span> <span></span> </a>
                 <div className="textoRotar"><a className="linkDecor" onClick={selOpt}>Let's talk</a></div>
+                <div className="logomovil"><img  src="./img/logopr.png" alt="Logo"></img></div>
             </div>
         </>
     )
