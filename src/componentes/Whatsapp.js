6b8898bf29a3f7583111ced161c9c436676ce7a4
anthony <tony.anthony@hotmail.es>
2023-05-20 20:57:28 -0500

    Wp PopUp

--- src/componentes/Whatsapp.js
+++ src/componentes/Whatsapp.js
@@ -1,7 +1,19 @@
 import React from 'react';
-import { ElfsightWidget } from 'react-elfsight-widget';
+/* import { ElfsightWidget } from 'react-elfsight-widget'; */
 
 function Whatsapp() {
-  return <ElfsightWidget widgetID="43f6364f-576b-4011-b140-97e7fda5230a" />;
+ /*  return <ElfsightWidget widgetID="43f6364f-576b-4011-b140-97e7fda5230a" />; */
+ return (
+  <>
+     <div class="fixed-tabs-Whats">
+                <div class="aba-whatsphone">
+                    <div class="aba-whatsphone-icone">
+                        <a target="_blank" href="https://api.whatsapp.com/send?phone=593959792827">Viaja<br/><strong>Con nosotros</strong></a>
+                    </div>
+                </div>
+            </div>
+  </>
+
+);  
 }
 export default Whatsapp;
